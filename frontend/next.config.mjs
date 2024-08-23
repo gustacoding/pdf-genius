9ff5df59dc41f6891be5aedd8b654d66a92c7d44
Gustavo Tatsch <gustatsch@gmail.com>
2024-08-23 16:33:03 -0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default nextConfig;
